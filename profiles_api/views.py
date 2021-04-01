e572def6185b30a42a3edc0914eaee729f2e0cc4
IS <ivoseeba@gmail.com>
2021-04-01 19:18:24 +0300

    Added post, put, patch and delete methods to our apiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,14 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+# from rest_framework.settings import api_settings
+# from rest_framework.permissions import IsAuthenticated
 
-#from profiles_api import serializers
-#from profiles_api import models
-#from profiles_api import permissions
+from profiles_api import serializers
+# from profiles_api import models
+# from profiles_api import permissions
 
 class HelloApiView(APIView):
 	"""Hello world API View"""
+	serializers_class = serializers.HelloSerializer
 
 	def get(self, request, format=None):
 		"""Return a list of APIView features"""
@@ -21,17 +23,27 @@ class HelloApiView(APIView):
 		return Response({'message':'Hello!','an_apiview':an_apiview})
 
 	def post(self, request, format=None):
-		"""Create a new item"""
-		return Response({'new': 'item'})
+		"""Create a hello message with our name"""
+		serializer = self.serializers_class(data=request.data)
+
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f'Hello {name}!'
+			return Response({'message': message})
+		else:
+			return Response(
+				serializer.errors,
+				status=status.HTTP_400_BAD_REQUEST
+			)
 
 	def put(self, request, format=None):
 		"""Update an item"""
-		return Response({'updated': 'item'})
+		return Response({'method': 'PUT'})
 
 	def patch(self, request, format=None):
 		"""Partially update an item"""
-		return Response({'partially_updated': 'item'})
+		return Response({'method': 'PATCH'})
 
 	def delete(self, request, format=None):
 		"""Delete an item"""
-		return Response({'deleted': 'item'})
+		return Response({'method': 'DELETE'})
